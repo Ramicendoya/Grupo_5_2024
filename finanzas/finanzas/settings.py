8aff9d0085d34ba90f383d8eac18e73b388e7c33
Manuel Morullo <manuelmorullo@hotmail.com>
2024-09-29 21:46:39 -0300

    configuro bd para mysql

--- finanzas/finanzas/settings.py
+++ finanzas/finanzas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'finanzas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nombre_de_la_base_de_datos',  # Reemplaza con el nombre de tu base de datos
+        'USER': 'nombre_de_usuario',  # Reemplaza con tu usuario de MySQL
+        'PASSWORD': 'contraseña',  # Reemplaza con tu contraseña de MySQL
+        'HOST': 'localhost',  # O la IP de tu servidor MySQL
+        'PORT': '3306',  # Puerto por defecto para MySQL
     }
 }
 
