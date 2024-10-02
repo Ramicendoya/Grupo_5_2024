cb7f098332597fb7709477b6bce5044b63588d0c
Rama <cendoyaramiro36@gmail.com>
2024-10-01 21:42:55 -0300

    Implementacion de navs

--- finanzas/app/urls.py
+++ finanzas/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('prueba/', views.Prueba, name='prueba'),
 ]
--- finanzas/app/views.py
+++ finanzas/app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html') 
+
+def Prueba(request):
+    return render(request, 'prueba2.html') 
