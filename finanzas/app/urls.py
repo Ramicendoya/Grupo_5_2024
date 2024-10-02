8ebec6f3eb06a47d42caedd0b5e4ddbc68bca0ac
Manuel Morullo <manuelmorullo@hotmail.com>
2024-10-02 14:31:45 -0300

    feat: agrego logica para que se pueda eliminar un ingreso

--- finanzas/app/urls.py
+++ finanzas/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import IngresoView
+from .views import IngresoView, EliminarIngreso
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,6 +10,6 @@ urlpatterns = [
     # Registrar Ingresos
     path('registrar/ingreso/', IngresoView.as_view(), name='registrar_ingreso'),
 
-    
+    path('registrar/ingreso/<int:ingreso_pk>/eliminar', EliminarIngreso.as_view(), name='eliminar_ingreso'),
 
 ]
--- finanzas/app/views.py
+++ finanzas/app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Ingreso, Persona, Categoria
 from django.utils import timezone
 from django.views import View
@@ -46,3 +48,22 @@ class IngresoView(View):
         ingreso.save()
 
         return redirect('registrar_ingreso')
+    
+class EliminarIngreso(View):
+    def post(self, request, ingreso_pk):
+
+        # Esto tenemos que reemplazarlo por la persona autenticada en la aplicacion
+        persona = get_object_or_404(Persona, pk=1)
+        
+        # Busca el ingreso por la pk que se pasa en la URL
+        ingreso = get_object_or_404(Ingreso, pk=ingreso_pk)
+
+        # Verifico si el ingreso pertenece a la persona
+        if ingreso.persona == persona:
+            ingreso.delete()
+            messages.success(request, "Ingreso eliminado con éxito.")
+        else:
+            messages.error(request, "El ingreso no pertenece a la persona especificada.")
+
+        return redirect('registrar_ingreso')
+    
