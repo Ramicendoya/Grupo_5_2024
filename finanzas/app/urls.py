c15892e76ccf5e7550df7f150b68ac0ca80a109d
Manuel Morullo <manuelmorullo@hotmail.com>
2024-10-02 03:19:14 -0300

    feat: agrego logica para que se listen los ingresos

--- finanzas/app/urls.py
+++ finanzas/app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from .views import IngresoView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ingresos/', views.ingresos, name='ingresos'),
     path('gastos/', views.gastos, name='gastos'),
+
+
+    # Registrar Ingresos
+    path('registrar/ingreso/', IngresoView.as_view(), name='registrar_ingreso'),
+
+    
+
 ]
--- finanzas/app/views.py
+++ finanzas/app/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Ingreso, Persona, Categoria
+from django.utils import timezone
+from django.views import View
 
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html') 
 
-def ingresos(request):
-    return render(request, 'ingresos.html') 
-
 def gastos(request):
-    return render(request, 'gastos.html') 
+    return render(request, 'gastos.html')
+
+
+
+#    Ingresos:
+
+class IngresoView(View):
+    def get(self, request):
+        # Obtiene la lista de ingresos fijos y variables
+        ingresos_fijos = Ingreso.objects.filter(fijo=True)
+        ingresos_variables = Ingreso.objects.filter(fijo=False)
+        
+        # Crea un contexto para pasar los ingresos a la plantilla
+        context = {
+            'ingresos_fijos': ingresos_fijos,
+            'ingresos_variables': ingresos_variables,
+        }
+        return render(request, 'ingresos.html', context)
+
+    def post(self, request):
+
+        descripcion = request.POST.get('descripcion')
+        monto = request.POST.get('monto')
+        fijo = 'tipo_ingreso' in request.POST
+
+        persona = Persona.objects.first() 
+
+        ingreso = Ingreso(
+            persona=persona,
+            fecha=timezone.now(), 
+            monto=monto,
+            descripcion=descripcion,
+            fijo=fijo,
+        )
+        ingreso.save()
+
+        return redirect('registrar_ingreso')
